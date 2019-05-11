5241a8ba70ae84b8757fd984914b29d24728e585
march-dave <ysangy@gmail.com>
2019-05-11 10:48:28 -0400

    cors

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
         }
       }
     axios
-    .get("https://api-comments.azurewebsites.net/api/Comments", config)
+    .get("https://api-comments.azurewebsites.net/api/Comments")
       .then(response => {
         console.log(JSON.stringify(response.data));
 
